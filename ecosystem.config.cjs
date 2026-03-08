0ed59cca48b2daa5bd89b8e8d1a5f87062968027
Fajri Rinaldi Chan <fajri@gariskode.com>
2026-03-08 10:20:07 +0700

    fix: update ecosystem configuration to use interpreter_args instead of env_file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,12 +3,12 @@ module.exports = {
     {
       name: 'personal-web',
       script: '.output/server/index.mjs',
+      interpreter_args: '--env-file=.env',
       instances: 1,
       exec_mode: 'fork',
       autorestart: true,
       watch: false,
       max_memory_restart: '512M',
-      env_file: '.env',
       env: {
         NODE_ENV: 'production',
         HOST: '0.0.0.0',
